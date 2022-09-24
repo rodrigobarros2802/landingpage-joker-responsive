6abc0dbc4df5a201cfef78b5ee33a7830d651133
Rodrigo Barros <rodrigobarros2802@gmail.com>
2022-09-24 15:33:30 -0300

    Incluindo video pause

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 function toggle() {
     var trailer = document.querySelector('.trailer');
-    var close = document.querySelector('.close');
+    var video = document.querySelector('video');
     trailer.classList.toggle('active');
+    video.currentTime = 0;
+    video.pause();
 }
